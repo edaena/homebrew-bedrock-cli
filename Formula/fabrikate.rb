f5ab0687d65e16b0b1e30802f5d38a725300f4aa
Edaena Salinas <edaena@users.noreply.github.com>
2020-04-28 18:41:11 -0700

    Update fabrikate.rb

--- Formula/fabrikate.rb
+++ Formula/fabrikate.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Fabrikate < Formula
     desc "Making GitOps with Kubernetes easier one component at a time"
     homepage "https://github.com/microsoft/fabrikate"
