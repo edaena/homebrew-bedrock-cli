e8ce12c6ae94c79c0bcf2b97a534a03a36e2bb72
Edaena Salinas <edaena@Edaenas-MacBook-Pro.local>
2020-05-08 11:20:25 -0700

    Change formula to not build from source

--- Formula/bedrock-cli.rb
+++ Formula/bedrock-cli.rb
@@ -1,29 +1,22 @@
 class BedrockCli < Formula
     desc "The CLI for Bedrock"
     homepage "https://microsoft.github.io/bedrock-cli/commands/"
-    url "https://github.com/microsoft/bedrock-cli/archive/v0.6.4.tar.gz"
-    sha256 "e8e3298795217e47a419dffa9d3e163b4c85da5fc57596822ec7d80f99e97519"
+    url "https://github.com/edaena/bedrock-cli/releases/download/v0.6.6/dist.tar.gz", :using => :curl
+    sha256 "32432b2a3ab1d984c67ed2330e37a7c6da7d6d8c09e5d09c434032cebdd4b058"
   
-    depends_on "yarn" => :build
-    depends_on "node" => :build
     depends_on "terraform"
     depends_on "helm" => "2.16.3"
     depends_on "azure-cli"
     depends_on "kubectl"
-    depends_on "edaena/core/fabrikate"
-  
+    
     def install
-      system "yarn", "install"
-      system "yarn", "build"
-  
       if OS.mac?
-        mv "./dist/bedrock-macos", "./dist/bedrock"
-        bin.install "./dist/bedrock"
+        mv "./bedrock-macos", "./bedrock"
       elsif OS.linux?
-        mv "./dist/bedrock-linux", "./dist/bedrock"
-        bin.install "./dist/bedrock"
+        mv "./bedrock-linux", "./bedrock"
       end
-    end
+      bin.install "./bedrock"
+    end 
 
     test do
       # Run the test with `brew test bedrock-cli`.
