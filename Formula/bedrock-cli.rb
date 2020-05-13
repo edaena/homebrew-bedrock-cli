fcedc716e6bad70be2046752235317e6b033d5f4
Edaena Salinas <edaena@Edaenas-MBP.hsd1.ca.comcast.net>
2020-05-13 10:45:42 -0700

    revert

--- Formula/bedrock-cli.rb
+++ Formula/bedrock-cli.rb
@@ -1,26 +1,32 @@
 class BedrockCli < Formula
-    desc "The CLI for Bedrock"
-    homepage "https://microsoft.github.io/bedrock-cli/commands/"
-    url "https://github.com/edaena/bedrock-cli/releases/download/v0.6.10/dist.tar.gz", :using => :curl
-    sha256 "ee9d8712632bdaca33991aa20a1404bdfb1631c1c92b5a1029894b6b419a6def"
-  
-    depends_on "terraform"
-    depends_on "helm" => "2.16.3"
-    depends_on "azure-cli"
-    depends_on "kubectl"
-    
-    def install
-      if OS.mac?
-        mv "./bedrock-macos", "./bedrock"
-      elsif OS.linux?
-        mv "./bedrock-linux", "./bedrock"
-      end
-      bin.install "./bedrock"
-    end 
+  desc "The CLI for Bedrock"
+  homepage "https://microsoft.github.io/bedrock-cli/commands/"
+  url "https://github.com/microsoft/bedrock-cli/archive/v0.6.4.tar.gz"
+  sha256 "e8e3298795217e47a419dffa9d3e163b4c85da5fc57596822ec7d80f99e97519"
 
-    test do
-      # Run the test with `brew test bedrock-cli`.
-      system "#{bin}/bedrock", "project", "init"
-      assert File.file?("bedrock.yaml")
+  depends_on "yarn" => :build
+  depends_on "node" => :build
+  depends_on "terraform"
+  depends_on "helm" => "2.16.3"
+  depends_on "azure-cli"
+  depends_on "kubectl"
+
+  def install
+    system "yarn", "install"
+    system "yarn", "build"
+
+    if OS.mac?
+      mv "./dist/bedrock-macos", "./dist/bedrock"
+      bin.install "./dist/bedrock"
+    elsif OS.linux?
+      mv "./dist/bedrock-linux", "./dist/bedrock"
+      bin.install "./dist/bedrock"
     end
   end
+
+  test do
+    # Run the test with `brew test bedrock-cli`.
+    system "#{bin}/bedrock", "project", "init"
+    assert File.file?("bedrock.yaml")
+  end
+end
