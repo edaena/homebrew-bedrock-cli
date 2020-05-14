005017f19b4efc05c2eb96c30af06c93474bf208
Automated Account <admin@azuredevops.com>
2020-05-14 17:47:32 +0000

    Update formula with new release.

--- Formula/bedrock-cli.rb
+++ Formula/bedrock-cli.rb
@@ -2,7 +2,7 @@ class BedrockCli < Formula
   desc "The CLI for Bedrock"
   homepage "https://microsoft.github.io/bedrock-cli/commands/"
   url "https://github.com/edaena/bedrock-cli/releases/download/v0.6.10/dist.tar.gz", :using => :curl
-  sha256 "ee9d8712632bdaca33991aa20a1404bdfb1631c1c92b5a1029894b6b419a6def"
+  sha256 "81e970300294ca88c6f6c5fa6cfd14713ad585190fd6f93244111c1901415987"
 
   depends_on "terraform"
   depends_on "helm" => "2.16.3"
